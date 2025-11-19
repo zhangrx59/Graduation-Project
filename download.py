c50b452577f21a89bb47dff3816994bdaa712f98
zhangrx59 <1324562976@qq.com>
2025-11-19 19:42:31 +0800

    修改分支名master->call_api

--- download.py
+++ download.py
@@ -1,6 +1,12 @@
+# import kagglehub
+#
+# # Download latest version
+# path = kagglehub.dataset_download("kmader/skin-cancer-mnist-ham10000")
+#
+# print("Path to dataset files:", path)
 import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("kmader/skin-cancer-mnist-ham10000")
+path = kagglehub.dataset_download("mahdavi1202/skin-cancer")
 
 print("Path to dataset files:", path)
